ede6eaf1020394d036f0bcb6b6bccea55200facf
öyinlade <aramideoyinlade960@gmail.com>
2025-05-24 16:08:54 +0100

    add all changes

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const languages = ["C is fun", "Phyton is cool", "JavaScripit is amazing"];
+const languages = ["C is fun", "Python is cool", "JavaScripit is amazing"];
 
 for (let i = 0; i < languages.length; i++) {
     console.log(languages[i]);
